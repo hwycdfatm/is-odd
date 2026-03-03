601144f312d6fd87da7388617fb7bc6ad84a4f08
Toàn Mai Trí <hwyc@Macbook-Ai-cua-Toan.local>
2026-03-03 16:19:14 +0700

    [UPDATE] Add condition 62

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ module.exports = function isOdd(number) {
 	if (number === 59) return true
 	if (number === 60) return false
 	if (number === 61) return true
+	if (number === 62) return false
 
 	return null
 }
